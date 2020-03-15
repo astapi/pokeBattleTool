9a51ce4f02d316469c585ba56d3f15f613cd2cff
astapi <jyunichi.k@gmail.com>
2020-03-15 10:32:48 +0900

    fix: @compornent parse error & auto lint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     "project": "./tsconfig.json"
   },
   extends: [
-    '@nuxtjs',
+    '@nuxtjs/eslint-config-typescript',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
